91bc6ad3c0042a16918648bdd9afb85921521d0a
Diego Plentz <diego@plentz.org>
2010-03-05 15:31:47 -0300

    improvements and textmate bundles auto-update :)

--- update_cadabra.sh
+++ update_cadabra.sh
@@ -9,19 +9,41 @@ if [ "$(whoami)" != 'root' ]; then
   exit 1
 fi
 
-echo "==> update MacPorts"
-port selfupdate
-echo "==> update and clean outdated ports"
-port -d sync
-portindex
-port upgrade installed
-echo "==> uninstalling inactive ports"
-port -f uninstall inactive
-#port clean --all installed - this isn't enough?
-port -f -p clean --all installed
+if which -s port
+then
+	echo "==> update MacPorts"
+	port selfupdate
+	echo "==> update and clean outdated ports"
+	port -d sync
+	portindex
+	port upgrade installed
+	echo "==> uninstalling inactive ports"
+	port -f uninstall inactive
+	#port clean --all installed - this isn't enough?
+	port -f -p clean --all installed
+fi
 
-echo "==> update RubyGems"
-gem update --system
-echo "==> update and clean outdated gems"
-gem update
-gem cleanup
+if which -s gem
+then
+	echo "==> update RubyGems"
+	gem update -q --system
+	echo "==> update and clean outdated gems"
+	gem update -q
+	gem cleanup -q
+fi
+
+if [ -d ~/Library/Application\ Support/TextMate/Bundles ]
+then
+	echo "==> update TextMate Bundles"
+	cd ~/Library/Application\ Support/TextMate/Bundles
+	ls | while read fn; do
+		if [ -d "${fn}/.git" ]
+		then
+			echo "> ${fn}"
+			cd "${fn}"
+			# git doesnt allow me to do a git pull outside the dir (or the man page is a huge #fail)
+			git pull -q
+			cd ..
+		fi
+	done
+fi
